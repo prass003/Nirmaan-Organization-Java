723f03e930c82dd9f2b5165979d0ced4de9ca392
prass003 <148111696+prass003@users.noreply.github.com>
2025-04-03 15:37:32 +0530

    Day2
    Data types

---
+++ Variabletask.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class Variabletask {
+
+	public static void main(String[] args) {
+		byte a = 22;
+		short b = 32000;
+		int c = 2000000;
+		long d = 7643773877l;
+		double e = 23.45565567;
+		float f = 23.45f;
+		boolean g = true;
+		      
+		System.err.println("byte value "+  a);
+		System.err.println("Short value " + b);
+		System.err.println("int value " + c);
+		System.err.println("long value "+d);
+		System.err.println("double value "+e);
+		System.err.println("float value "+f);
+		System.err.println("Boolean value "+g);
+	}
+
+}
