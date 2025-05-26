f0f4a87fef74ec9cdb214a8b0986799baad67394
prass003 <148111696+prass003@users.noreply.github.com>
2025-05-26 11:23:40 +0530

    day21
    Wrapperclass

---
+++ WrapperClass.java
@@ -0,0 +1,48 @@
+package day21;
+
+public class WrapperClass {
+     
+	
+	
+	public static void main(String[] args) {
+		System.out.println("*****unBoxing");
+		Integer num=10;
+		int num2=num;
+		System.out.println(num2);
+		Float f=2.5f;
+		float f2=f;
+		System.out.println(f2);
+		Double d=2.6476737363;
+		double d2=d;
+		System.out.println(d2);
+		Boolean iswork=false;
+		boolean iswork2=iswork;
+		System.out.println(iswork2);
+		Byte age=8;
+		byte age2=age;
+		System.out.println(age2);
+		Short s=2434;
+		short s2=s;
+		System.out.println(s2);
+		System.out.println("***autoboxing****");
+		int n2=10;
+		Integer n=n2;
+		System.out.println(num);
+		float f1=4.2f;
+		Float f3=f1;
+		System.out.println(f3);
+		double d3=3.655675754;
+		Double d1=d3;
+		System.out.println(d1);
+		boolean b=true;
+		Boolean b2=b;
+		System.out.println(b2);
+		byte a2=20;
+		Byte a1=a2;
+		System.out.println(a1);
+		short s1=4363;
+		Short s3=s1;
+		System.out.println(s3);
+	}
+
+}
