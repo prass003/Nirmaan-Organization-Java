b1088a14fceeeb95b2bfa6d1f26dea309c83dd69
prass003 <148111696+prass003@users.noreply.github.com>
2025-05-26 11:22:54 +0530

    day22
    Arraylist

---
+++ Collections.java
@@ -0,0 +1,24 @@
+package day22;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		List<Object> al = new ArrayList<>();
+		al.add(12);
+		al.add("Prasanna");
+		al.add(true);
+		al.addFirst(301);
+		al.addLast(201);
+		al.contains(al);
+		al.get(3);
+		al.getFirst();
+		al.getLast();
+		al.remove(0);
+
+	}
+
+}
